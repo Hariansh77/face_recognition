d940a9cc3ecf214d0e8676f7152bc8e74ce52ee6
Hariansh77 <patelhariansh@gmail.com>
2024-06-28 17:18:20 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+# Create a CSV file for attendance records
+# csv_file = open(r"C:\Users\Dhruv Patel\OneDrive\Desktop\face recognition\hariansh.csv", mode='a', newline='')
+# csv_writer = csv.writer(csv_file)
